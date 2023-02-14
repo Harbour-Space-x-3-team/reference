c0e126a101e29b48d5ca69da18f414158278898e
Brayan Durán Medina <brayan.duran@leaguesofcode.com>
2023-02-14 22:34:01 +0100

    diophantine updated

--- content/number-theory/Diophantine.cpp
+++ content/number-theory/Diophantine.cpp
@@ -1,5 +1,5 @@
 template<typename T>
-bool diophantine(T a, T b, T c, T &x, T &y, T &g) {
+bool find_any_solution(T a, T b, T c, T &x, T &y, T &g) {
     if (a == 0 && b == 0) {
         if (c == 0) {
             x = y = g = 0;
@@ -31,4 +31,44 @@ bool diophantine(T a, T b, T c, T &x, T &y, T &g) {
     g = abs(g);
     return true;
     // |x|, |y| <= max(|a|, |b|, |c|) [tested]
+}
+
+template<typename T>
+void shift_solution(T & x, T & y, T a, T b, T cnt) {
+    x += cnt * b, y -= cnt * a;
+}
+
+template<typename T>
+T find_all_solutions(T a, T b, T c, T minx, T maxx, T miny, T maxy) {
+    T x, y, g;
+    if (!find_any_solution(a, b, c, x, y, g))
+        return 0;
+    a /= g, b /= g;
+    T sign_a = a > 0 ? +1 : -1, sign_b = b > 0 ? +1 : -1;
+    shift_solution(x, y, a, b, (minx - x) / b);
+    if (x < minx)
+        shift_solution(x, y, a, b, sign_b);
+    if (x > maxx)
+        return 0;
+    T lx1 = x;
+    shift_solution(x, y, a, b, (maxx - x) / b);
+    if (x > maxx)
+        shift_solution(x, y, a, b, -sign_b);
+    T rx1 = x;
+    shift_solution(x, y, a, b, -(miny - y) / a);
+    if (y < miny)
+        shift_solution(x, y, a, b, -sign_a);
+    if (y > maxy)
+        return 0;
+    T lx2 = x;
+    shift_solution(x, y, a, b, -(maxy - y) / a);
+    if (y > maxy)
+        shift_solution(x, y, a, b, sign_a);
+    T rx2 = x;
+    if (lx2 > rx2)
+        swap(lx2, rx2);
+    T lx = max(lx1, lx2), rx = min(rx1, rx2);
+    if (lx > rx)
+        return 0;
+    return (rx - lx) / abs(b) + 1;
 }
