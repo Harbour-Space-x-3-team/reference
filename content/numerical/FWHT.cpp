c6cb6ea6397740dfc4843bb8ea960e22280b8217
Anier Velasco Sotomayor <anier.velasco@gmail.com>
2023-02-15 17:14:05 +0100

    Trimming shit.

--- content/numerical/FWHT.cpp
+++ content/numerical/FWHT.cpp
@@ -1,13 +1,3 @@
-/**
- * Author: Lucian Bicsi
- * Date: 2015-06-25
- * License: GNU Free Documentation License 1.2
- * Source: csacademy
- * Description: Transform to a basis with fast convolutions of the form
- * $\displaystyle c[z] = \sum\nolimits_{z = x \oplus y} a[x] \cdot b[y]$,
- * where $\oplus$ is one of AND, OR, XOR. The size of $a$ must be a power of two.
- * Time: O(N \log N)
- */
 #pragma once
 
 // Notes: if you use mod make sure 0 <= a[i], b[i] < mod when you call convolve
